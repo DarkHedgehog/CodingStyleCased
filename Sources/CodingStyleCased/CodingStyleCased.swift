f30484e8db65e9dfbe7112f760edbb4673258083
Derevenskikh Aleksander <hedgehog@kartis.ru>
2023-02-05 19:22:21 +0300

    try to fix internal access level

--- Sources/CodingStyleCased/CodingStyleCased.swift
+++ Sources/CodingStyleCased/CodingStyleCased.swift
@@ -1,57 +1,55 @@
+//public struct CodingStyleCased {
+//    public private(set) var text = "Hello, World!"
+//
+//    public init() {
+//    }
+//}
+//
+//public struct CodingStyleCasedTest1 {
+//    public private(set) var text = "Hello, World!"
+//
+//    public init() {
+//    }
+//}
+
+
+@propertyWrapper
 public struct CodingStyleCased {
-    public private(set) var text = "Hello, World!"
+    var value: String
+    var style: Style
 
-    public init() {
+    init(wrappedValue: String, _ style: Style) {
+        self.style = style
+        self.value = wrappedValue
     }
 
-    @propertyWrapper
-    public struct CodingStyle {
-        var value: String
-        var style: Style
-
-        init(wrappedValue: String, _ style: Style) {
-            self.style = style
-            self.value = wrappedValue
+    public var wrappedValue: String {
+        get {
+            return value
         }
-
-        public var wrappedValue: String {
-            get {
-                return value
-            }
-            set {
-                value = style.styling(newValue)
-                print("set to " + value)
-            }
+        set {
+            value = style.styling(newValue)
+            print("set to " + value)
         }
+    }
 
-        public enum Style {
-            /// camelCaseExample
-            case camelCase
-            /// snake_case_example
-            case snakeCase
-            /// kebab-case-example
-            case kebabCase
+    public enum Style {
+        /// camelCaseExample
+        case camelCase
+        /// snake_case_example
+        case snakeCase
+        /// kebab-case-example
+        case kebabCase
 
-            func styling (_ caseValue: String) -> String {
-                switch self {
-                case .camelCase:
-                    return caseValue.camelCased
-                case .kebabCase:
-                    return caseValue.kebabCased
-                case .snakeCase:
-                    return caseValue.snakeCased
-                }
+        func styling (_ caseValue: String) -> String {
+            switch self {
+            case .camelCase:
+                return caseValue.camelCased
+            case .kebabCase:
+                return caseValue.kebabCased
+            case .snakeCase:
+                return caseValue.snakeCased
             }
         }
     }
 }
-
-public struct CodingStyleCasedTest1 {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
-
-
-
