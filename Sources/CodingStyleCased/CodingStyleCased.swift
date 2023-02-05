58fd4b09618c43004c031b41cb6c833eb715eba1
Derevenskikh Aleksander <hedgehog@kartis.ru>
2023-02-05 19:06:25 +0300

    try to fix

--- Sources/CodingStyleCased/CodingStyleCased.swift
+++ Sources/CodingStyleCased/CodingStyleCased.swift
@@ -1,6 +1,48 @@
-//public struct CodingStyleCased {
-//    public private(set) var text = "Hello, World!"
-//
-//    public init() {
-//    }
-//}
+public struct CodingStyleCased {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
+
+
+@propertyWrapper
+struct CodingStyle {
+    var value: String
+    var style: Style
+
+    init(wrappedValue: String, _ style: Style) {
+        self.style = style
+        self.value = wrappedValue
+    }
+
+    var wrappedValue: String {
+        get {
+            return value
+        }
+        set {
+            value = style.styling(newValue)
+            print("set to " + value)
+        }
+    }
+
+    enum Style {
+        /// camelCaseExample
+        case camelCase
+        /// snake_case_example
+        case snakeCase
+        /// kebab-case-example
+        case kebabCase
+
+        func styling (_ caseValue: String) -> String {
+            switch self {
+            case .camelCase:
+                return caseValue.camelCased
+            case .kebabCase:
+                return caseValue.kebabCased
+            case .snakeCase:
+                return caseValue.snakeCased
+            }
+        }
+    }
+}
--- Tests/CodingStyleCasedTests/CodingStyleCasedTests.swift
+++ Tests/CodingStyleCasedTests/CodingStyleCasedTests.swift
@@ -2,6 +2,7 @@ import XCTest
 @testable import CodingStyleCased
 
 final class CodingStyleCasedTests: XCTestCase {
+    @CodingStyle(.camelCase) var camelString = "hasdfk dsa fdslhf adsl-sdgsdg_dsag"
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
