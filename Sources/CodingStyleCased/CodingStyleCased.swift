d01b5c3b602b08a701db17a3a045d5726a84af66
Derevenskikh Aleksander <hedgehog@kartis.ru>
2023-02-05 19:24:16 +0300

    fix access levels

--- Sources/CodingStyleCased/CodingStyleCased.swift
+++ Sources/CodingStyleCased/CodingStyleCased.swift
@@ -1,9 +1,9 @@
-//public struct CodingStyleCased {
-//    public private(set) var text = "Hello, World!"
-//
-//    public init() {
-//    }
-//}
+public struct CodingStyleCased {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
 //
 //public struct CodingStyleCasedTest1 {
 //    public private(set) var text = "Hello, World!"
@@ -12,44 +12,3 @@
 //    }
 //}
 
-
-@propertyWrapper
-public struct CodingStyleCased {
-    var value: String
-    var style: Style
-
-    init(wrappedValue: String, _ style: Style) {
-        self.style = style
-        self.value = wrappedValue
-    }
-
-    public var wrappedValue: String {
-        get {
-            return value
-        }
-        set {
-            value = style.styling(newValue)
-            print("set to " + value)
-        }
-    }
-
-    public enum Style {
-        /// camelCaseExample
-        case camelCase
-        /// snake_case_example
-        case snakeCase
-        /// kebab-case-example
-        case kebabCase
-
-        func styling (_ caseValue: String) -> String {
-            switch self {
-            case .camelCase:
-                return caseValue.camelCased
-            case .kebabCase:
-                return caseValue.kebabCased
-            case .snakeCase:
-                return caseValue.snakeCased
-            }
-        }
-    }
-}
